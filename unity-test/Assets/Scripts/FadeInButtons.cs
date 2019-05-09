237cd12c0c5d37309a7bda016610916180c9e3d8
Erika Wolfe <eywolfe@cs.uw.edu>
2019-05-08 17:43:29 -0700

    Add flyer navigation and fade choice buttons

---
+++ unity-test/Assets/Scripts/FadeInButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInButtons : MonoBehaviour
+{
+    private GameObject[] buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons = GameObject.FindGameObjectsWithTag("Choice Button");
+        foreach (GameObject button in buttons)
+        {
+            button.SetActive(false);
+        }
+        Invoke("ActiveButtons", 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ActiveButtons()
+    {
+        foreach (GameObject button in buttons)
+        {
+            button.SetActive(true);
+        }
+    }
+}
