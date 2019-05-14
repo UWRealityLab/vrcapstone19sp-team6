c449d278be18a342a6e8e5c110f22510ec79ee16
Erika Wolfe <eywolfe@cs.uw.edu>
2019-05-14 00:59:09 -0700

    Add new navigation system (gaze menu, gaze buttons, configurable script per scene)

---
+++ FindingSeattleMVPGazeOnly/Assets/Scripts/AnimatedText.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedText : MonoBehaviour
+{
+    private enum State { START, ENTERED, EXITED };
+    private State state;
+
+    private Vector3 finalPosition;
+    private Color finalColor;
+
+    private void Start()
+    {
+        state = State.START;
+
+        finalPosition = transform.position;
+        Vector3 startPosition = finalPosition;
+        startPosition.y -= 10.0f;
+        transform.position = startPosition;
+
+        finalColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.clear;
+    }
+
+    public void Enter(float duration)
+    {
+        if (state == State.START)
+        {
+            state = State.ENTERED;
+            StartCoroutine(RiseFadeEntrance(finalPosition, finalColor, duration));
+        }
+    }
+
+    public void Exit(float duration)
+    {
+        if (state == State.ENTERED)
+        {
+            state = State.EXITED;
+            StartCoroutine(RiseFadeExit(finalPosition, finalColor, duration));
+        }
+    }
+
+    IEnumerator RiseFadeEntrance(Vector3 finalPosition, Color finalColor, float duration)
+    {
+        Vector3 startPosition = finalPosition;
+        startPosition.y -= 10.0f;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / duration)
+        {
+            Color newColor = new Color(finalColor.r, finalColor.g, finalColor.b, Mathf.Lerp(0.0f, 1.0f, Easing.EaseOutQuart(0.0f, 1.0f, t)));
+            GetComponent<Renderer>().material.color = newColor;
+            Vector3 newPosition = Vector3.Lerp(startPosition, finalPosition, Easing.EaseOutQuart(0.0f, 1.0f, t));
+            transform.position = newPosition;
+            yield return null;
+        }
+    }
+
+    IEnumerator RiseFadeExit(Vector3 startPosition, Color startColor, float duration)
+    {
+        Vector3 finalPosition = startPosition;
+        finalPosition.y += 6.0f;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / duration)
+        {
+            Color newColor = new Color(finalColor.r, finalColor.g, finalColor.b, Mathf.Lerp(1.0f, 0.0f, Easing.EaseInQuart(0.0f, 1.0f, t)));
+            GetComponent<Renderer>().material.color = newColor;
+            Vector3 newPosition = Vector3.Lerp(startPosition, finalPosition, Easing.EaseInQuart(0.0f, 1.0f, t));
+            transform.position = newPosition;
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+}
---
+++ FindingSeattleMVPGazeOnly/Assets/Scripts/GazeInteractable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeInteractable : MonoBehaviour
+{
+    public GameObject[] showOnGaze;
+    public GameObject[] hideOnGaze;
+    public GameObject[] showOnSelect;
+    public GameObject[] hideOnSelect;
+
+    protected virtual void Start()
+    {
+        GazeStop();
+        Deselect();
+    }
+
+    public virtual void Select()
+    {
+        foreach (GameObject obj in showOnSelect)
+        {
+            obj.SetActive(true);
+        }
+        foreach (GameObject obj in hideOnSelect)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public virtual void Deselect()
+    {
+        foreach (GameObject obj in showOnSelect)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in hideOnSelect)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    public virtual void GazeStart()
+    {
+        foreach (GameObject obj in showOnGaze)
+        {
+            obj.SetActive(true);
+        }
+        foreach (GameObject obj in hideOnGaze)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public virtual void GazeStop()
+    {
+        foreach (GameObject obj in showOnGaze)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in hideOnGaze)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    public virtual Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+    public virtual Quaternion GetRotation()
+    {
+        return transform.rotation * Quaternion.Euler(90, 0, 0);
+    }
+}
