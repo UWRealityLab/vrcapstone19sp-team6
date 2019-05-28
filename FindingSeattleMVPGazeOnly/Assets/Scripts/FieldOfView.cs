88ccaee6164625845ab1c1bd914b06897b8b6c39
Erika Wolfe <eywolfe@cs.uw.edu>
2019-05-28 13:35:15 -0700

    Add new FOV script

---
+++ FindingSeattleMVPGazeOnly/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    private Camera camera;
+    private float windowaspect;
+    private float targetaspect;
+    private float scaleheight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("entered start");
+        camera = GetComponent<Camera>();
+        windowaspect = (float)Screen.width / (float)Screen.height;
+        targetaspect = 12.0f / 9.0f;
+        scaleheight = windowaspect / targetaspect;
+        scale();
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+    void scale()
+    {
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        } 
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+        Debug.Log("scaled fine");
+
+    }
+}
