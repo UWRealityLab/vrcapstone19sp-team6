c3de7d4753ffaefa3e0b4de978f130b83c6ff547
Erika Wolfe <eywolfe@cs.uw.edu>
2019-06-04 15:22:42 -0700

    Add new script for timeline system

---
+++ FindingSeattleMVPGazeOnly/Assets/Scripts/FS_Timeline.cs
@@ -0,0 +1,117 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class FS_Timeline : MonoBehaviour
+//{
+//    void SetupStartScene()
+//    {
+
+//    }
+
+//    void SetupEvictionScene()
+//    {
+
+//    }
+
+//    void SetupMorningScene()
+//    {
+
+//    }
+
+//    void SetupParkScene()
+//    {
+
+//    }
+
+//    void SetupEveningScene()
+//    {
+
+//    }
+
+//    void SetupFinalScene()
+//    {
+
+//    }
+
+//    /**
+//     * ====================================================================================================
+//     *  Below this point are utility classes and infrastructure to make scene setup work
+//     * ====================================================================================================
+//     */
+
+//    private float ScriptPlayCompleteSegment(float time, float fadeDuration, string videoPath, float videoDuration, string audioPath, float audioDuration, float audioVolume)
+//    {
+
+//    }
+
+//    private float ScriptPlayAudioClip(float time, float audioPath, float audioDuration, float audioVolume)
+//    {
+
+//    }
+
+//    private float ScriptLoadSceneObjects(float time)
+//    {
+
+//    }
+
+//    private List<Event> timeline;
+
+//    private abstract class Event
+//    {
+//        public float time;
+//        public abstract void Start();
+//    }
+
+//    private class VideoEvent : Event
+//    {
+//        public override void Start()
+//        {
+
+//        }
+//    }
+
+//    private class AudioEvent : Event
+//    {
+//        public override void Start()
+//        {
+
+//        }
+//    }
+
+//    private class FadeEvent : Event
+//    {
+//        public override void Start()
+//        {
+
+//        }
+//    }
+
+//    private class AnimateTextEvent : Event
+//    {
+//        public override void Start()
+//        {
+
+//        }
+//    }
+
+//    private class LoadObjectEvent : Event
+//    {
+//        public override void Start()
+//        {
+
+//        }
+//    }
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
